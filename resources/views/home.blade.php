5b9f0710a403362f03564aa2733e54b9ebee5bc4
evanfadilahd <yiersanshefan@gmail.com>
2023-04-14 13:51:26 +0700

    Jumat 14_Generalization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,6 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php $i = $data ?>
                     @foreach ($data as $item)
                     <tr>
                         <td> {{ $item->request_id }} </td>
@@ -97,12 +96,46 @@
                         <td> {{ $item->create_at }} </td>
                         <td> {{ $item->updated_at }} </td>
                     </tr>
-                    <?php $i++ ?>
                     @endforeach
                 </tbody>
             </table>
             </div>
             <!-- AKHIR DATA -->
+
+            {{-- Sell --}}
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th class="col-md-1">sell_id</th>
+                        <th class="col-md-1">sell_username</th>
+                        <th class="col-md-1">sell_goods</th>
+                        <th class="col-md-1">sell_stock</th>
+                        <th class="col-md-1">sell_price</th>
+                        <th class="col-md-1">sell_desc</th>
+                        <th class="col-md-1">create_at</th>
+                        <th class="col-md-1">updated_at</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    @foreach ($data2 as $item)
+                    <tr>
+                        <td> {{ $item->sell_id }} </td>
+                        <td> {{ $item->sell_username }} </td>
+                        <td> {{ $item->sell_goods }} </td>
+                        <td> {{ $item->sell_stock }} </td>
+                        <td> {{ $item->sell_price }} </td>
+                        <td> {{ $item->sell_desc }} </td>
+                        <td> {{ $item->create_at }} </td>
+                        <td> {{ $item->updated_at }} </td>
+                    </tr>
+                    
+                    @endforeach
+                </tbody>
+            </table>
+            </div>
+            <!-- AKHIR DATA -->
+        
         </section>
     </body>
     
