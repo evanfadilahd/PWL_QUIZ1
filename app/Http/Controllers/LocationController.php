966b784b1b751af2707c8a4a5ae2479e4bfd2c43
evanfadilahd <92024300+evanfadilahd@users.noreply.github.com>
2023-04-17 12:17:09 +0700

    Revert "locationview"

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\tb_butchery;
-use App\Models\tb_land;
 
 class LocationController extends Controller
 {
@@ -15,24 +13,4 @@ class LocationController extends Controller
         return view('location.location');
     }
 
-    public function butchery()
-    {
-    //     $data = tb_request::orderBy('request_id', 'desc');
-    //     $data2 = tb_sell::orderBy('sell_id', 'desc');
-    //     return view('home')->with('data', $data)->with('data2', $data2);
-    $data = tb_butchery::all();
-        return view('location.butchery-detail', compact('data'));
-    }
-
-    public function land()
-    {
-    //     $data = tb_request::orderBy('request_id', 'desc');
-    //     $data2 = tb_sell::orderBy('sell_id', 'desc');
-    //     return view('home')->with('data', $data)->with('data2', $data2);
-    $data = tb_land::all();
-        return view('location.land-detail', compact('data'));
-    }
-
-
 }
-
